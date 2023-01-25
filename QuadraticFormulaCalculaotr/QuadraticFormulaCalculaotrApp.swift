a1c5896dc2b3037066e0ad36b194b3c46ee3393e
Cooper Lawson <CSLawson@lcs.on.ca>
2023-01-25 11:36:29 -0500

    Initial Commit

---
+++ QuadraticFormulaCalculaotr/QuadraticFormulaCalculaotrApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuadraticFormulaCalculaotrApp.swift
+//  QuadraticFormulaCalculaotr
+//
+//  Created by Cooper Lawson on 2023-01-25.
+//
+
+import SwiftUI
+
+@main
+struct QuadraticFormulaCalculaotrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
