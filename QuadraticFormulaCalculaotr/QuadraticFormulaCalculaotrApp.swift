e25b4dabcb24c561043417a541652afaac31de4b
Cooper Lawson <CSLawson@lcs.on.ca>
2023-01-25 11:44:07 -0500

    Setting up

--- QuadraticFormulaCalculaotr/QuadraticFormulaCalculaotrApp.swift
+++ QuadraticFormulaCalculaotr/QuadraticFormulaCalculaotrApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct QuadraticFormulaCalculaotrApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootsCalculatorView()
         }
     }
 }
---
+++ QuadraticFormulaCalculaotr/Views/RootsCalculatorView.swift
@@ -0,0 +1,30 @@
+//
+//  RootsCalculatorView.swift
+//  QuadraticFormulaCalculaotr
+//
+//  Created by Cooper Lawson on 2023-01-25.
+//
+
+import SwiftUI
+
+struct RootsCalculatorView: View {
+    //MARK Stored Properties
+    
+    //MARK Computed Properties
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+
+
+
+
+
+struct RootsCalculatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootsCalculatorView()
+    }
+}
